d208df76df71a5a2b0e38cf3d01697cdae4178b9
Angel Angelov <hextwoa@gmail.com>
2016-12-16 22:18:39 +0200

    add simple cli

--- lib/web/web.py
+++ lib/web/web.py
@@ -31,7 +31,7 @@ class StringGenerator(object):
         return cherrypy.session['mystring']
 
 
-if __name__ == '__main__':
+def main():
     conf = {
         '/': {
             'tools.staticdir.on': True,
@@ -42,3 +42,6 @@ if __name__ == '__main__':
         },
     }
     cherrypy.quickstart(StringGenerator(), '/', conf)
+
+if __name__ == '__main__':
+    main()
