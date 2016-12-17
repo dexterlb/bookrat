408b5d7e72b4a07834ac44d3132a8b2a5a304803
Angel Angelov <hextwoa@gmail.com>
2016-12-17 19:47:05 +0200

    fix ignored argument

--- bin/cli.py
+++ bin/cli.py
@@ -26,7 +26,7 @@ def import_books(db, dir):
 @click.command(help='import books from a folder')
 @click.option('--db', help='database URN', required=True)
 @click.argument('ignored')
-def count(db):
+def count(db, ignored):
     m = megatron.Megatron(db)
     counting_worker.run(m)
 
