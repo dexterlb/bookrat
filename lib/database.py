5b92821a3e50ffda656aa903abd4db93d96ea136
Angel Angelov <hextwoa@gmail.com>
2016-12-17 20:10:31 +0200

    import pycopg2

--- lib/database.py
+++ lib/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import pycopg2
+
 Base = declarative_base()
 class Database:
     def __init__(self, url):
