77d92b3d02c1bc44ab7873d60f1d6e81ee15f72f
Angel Angelov <hextwoa@gmail.com>
2016-12-17 21:42:34 +0200

    always add indices

--- lib/tf_idf.py
+++ lib/tf_idf.py
@@ -5,10 +5,15 @@ class TFIDF:
             self.megatron = megatron
 
     def compute_idf(self):
+            self.megatron.word_book_controller.add_indices()
+            self.megatron.tf_idf_controller.drop_tables()
+            self.megatron.tf_idf_controller.create_tables()
             self.megatron.tf_idf_controller.compute_idf()
 
     def compute_tfidf(self):
+            self.megatron.tf_idf_controller.add_idf_indices()
             self.megatron.tf_idf_controller.compute_tfidf()
 
     def compute_top_words(self):
+            self.megatron.tf_idf_controller.add_tfidf_indices()
             self.megatron.tf_idf_controller.compute_top_words()
